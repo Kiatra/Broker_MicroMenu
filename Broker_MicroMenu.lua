1f3dc58bc81f9e045f5a974bcc755e5e02a2cc75
yess <yess@xan.at>
2017-03-29 14:39:25 +0200

    toc update, fix help menu error

--- Broker_MicroMenu.lua
+++ Broker_MicroMenu.lua
@@ -22,7 +22,7 @@ local function Debug(...)
 end
 
 local function RGBToHex(r, g, b)
-	return ("%02x%02x%02x"):format(r*255, g*255, b*255)	
+	return ("%02x%02x%02x"):format(r*255, g*255, b*255)
 end
 
 local mb = _G.MainMenuMicroButton:GetScript("OnMouseUp")
@@ -33,8 +33,8 @@ dataobj = ldb:NewDataObject("Broker_MicroMenu", {
 	icon = path.."green.tga",
 	label = "MicroMenu",
 	text  = "",
-	OnClick = function(self, button, ...) 
-		if button == "RightButton" then 
+	OnClick = function(self, button, ...)
+		if button == "RightButton" then
 			if _G.IsModifierKeyDown() then
 				mainmenu(self, button, ...)
 			else
@@ -62,10 +62,10 @@ function cellPrototype:SetupCell(tooltip, value, justification, font, iconCoords
 	local tex = self.texture
 	tex:SetWidth(16)
 	tex:SetHeight(16)
-	
+
 	if guild then
 		_G.SetSmallGuildTabardTextures("player", tex,tex);
-	elseif unitID then 
+	elseif unitID then
 		_G.SetPortraitTexture(tex, unitID)
 	else
 		tex:SetTexture(value)
@@ -73,7 +73,7 @@ function cellPrototype:SetupCell(tooltip, value, justification, font, iconCoords
 	if iconCoords then
 		tex:SetTexCoord(_G.unpack(iconCoords))
 	end
-	return tex:GetWidth(), tex:GetHeight() 
+	return tex:GetWidth(), tex:GetHeight()
 end
 
 function cellPrototype:ReleaseCell()
@@ -114,7 +114,7 @@ function dataobj:UpdateText()
 			dataobj.icon = path.."red.tga"
 		end
 	end
-	
+
 	if db.customTextSetting then
 		local lw_string = colorWorld..latencyWorld.."|r"
 		local lh_string = colorHome..latencyHome.."|r"
@@ -135,7 +135,7 @@ function dataobj:UpdateText()
 			else
 				dataobj.text = string.format("%s%s%i|r fps", text, fpsColor, fps )
 			end
-		else 
+		else
 			dataobj.text = text
 		end
 	end
@@ -143,31 +143,31 @@ end
 
 local function MouseHandler(event, func, button, ...)
 	local name = func
-	
+
 	if _G.type(func) == "function" then
 		func(event, func,button, ...)
 	else
 		func:GetScript("OnClick")(func,button, ...)
 	end
-	
+
 	LibQTip:Release(tooltip)
 	tooltip = nil
 end
 
 function dataobj:OnEnter()
-	if tooltip then 
-		LibQTip:Release(tooltip) 
+	if tooltip then
+		LibQTip:Release(tooltip)
 	end
-	
+
 	tooltip = LibQTip:Acquire("Broker_MicroMenuTooltip", 2, "LEFT", "LEFT")
 	tooltip:Clear()
-	self.tooltip = tooltip 
+	self.tooltip = tooltip
 
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, "", myProvider, {0.2, 0.8, 0.2, 0.8},"player")
 	tooltip:SetCell(y, 2, _G.CharacterMicroButton.tooltipText)
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() _G.ToggleCharacter("PaperDollFrame") end)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, path.."spells.tga", myProvider)
 	--tooltip:SetCell(y, 2, "Spellbook & Abilities |cffffd200(P)|r")
@@ -177,8 +177,8 @@ function dataobj:OnEnter()
 	else
 		tooltip:SetCell(y, 2, _G.SPELLBOOK_ABILITIES_BUTTON)
 	end
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function(self, func, button, ...) 
-		
+	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function(self, func, button, ...)
+
 		if _G.InCombatLockdown() then
 			if key then
 				_G.DEFAULT_CHAT_FRAME:AddMessage("Cant' open the Spellbook during combat. Use your hot key: "..key)
@@ -198,12 +198,12 @@ function dataobj:OnEnter()
 			--]]
 		end
 	end)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, path.."talents.tga", myProvider)
 	tooltip:SetCell(y, 2, _G.TalentMicroButton.tooltipText)
 	--tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, TalentMicroButton)
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function(self, func, button, ...) 
+	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function(self, func, button, ...)
 		if _G.InCombatLockdown() then
 			key = _G.GetBindingKey("TOGGLETALENTS")
 			if key then
@@ -222,35 +222,35 @@ function dataobj:OnEnter()
 			end
 		end
 	end)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, path.."achivements.tga", myProvider)
 	tooltip:SetCell(y, 2, _G.AchievementMicroButton.tooltipText)
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.AchievementMicroButton)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, path.."quest.tga", myProvider)
 	tooltip:SetCell(y, 2, _G.QuestLogMicroButton.tooltipText)
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.QuestLogMicroButton)
-	
+
 	local y, x = tooltip:AddLine()
 	--tooltip:SetCell(y, 1, path.."social.tga", myProvider)
 	tooltip:SetCell(y, 1, "", myProvider, nil,"player",true)
 	tooltip:SetCell(y, 2, _G.GuildMicroButton.tooltipText)
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.GuildMicroButton)
-	
+
 	--[[
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, path.."pvp.tga", myProvider)
 	tooltip:SetCell(y, 2, "PvP")
 	--tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() ToggleFrame(PVPParentFrame) end)
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() 
+	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function()
 		if TogglePVPUI then
 			TogglePVPUI()
 		end
 	end)
 	]]
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, path.."lfg.tga", myProvider)
 	tooltip:SetCell(y, 2, _G.LFDMicroButton.tooltipText)
@@ -262,7 +262,7 @@ function dataobj:OnEnter()
 		tooltip:SetCell(y, 2, _G.CollectionsMicroButton.tooltipText)
 		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.CollectionsMicroButton)
 	end
-	
+
 	if _G.EJMicroButton then
 		local y, x = tooltip:AddLine()
 		tooltip:SetCell(y, 1, path.."journal.tga", myProvider)
@@ -275,43 +275,43 @@ function dataobj:OnEnter()
 		tooltip:SetCell(y, 2, _G.RaidMicroButton.tooltipText)
 		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.RaidMicroButton)
 	end
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, path.."green.tga", myProvider)
 	tooltip:SetCell(y, 2, _G.MainMenuMicroButton.tooltipText)
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, mainmenu)
-	
-	local y, x = tooltip:AddLine()
-	tooltip:SetCell(y, 1, path.."help.tga", myProvider)
-	tooltip:SetCell(y, 2, _G.HelpMicroButton.tooltipText)
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.HelpMicroButton)
-		
+
+	--local y, x = tooltip:AddLine()
+	--tooltip:SetCell(y, 1, path.."help.tga", myProvider)
+	--tooltip:SetCell(y, 2, _G.HelpMicroButton.tooltipText)
+	--tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.HelpMicroButton)
+
 	tooltip:AddSeparator(10,0,0,0,0)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 1, "", myProvider)
 	tooltip:SetCell(y, 2, _G.GameMenuButtonOptions:GetText())
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() _G.VideoOptionsFrame:Show() end)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 2, "Interface")
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() _G.InterfaceOptionsFrame:Show() end)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 2, _G.GameMenuButtonKeybindings:GetText())
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() _G.KeyBindingFrame_LoadUI(); _G.KeyBindingFrame:Show() end)
-	
+
 	local y, x = tooltip:AddLine()
 	tooltip:SetCell(y, 2, _G.GameMenuButtonMacros:GetText())
 	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.ShowMacroFrame)
-	
+
 	if _G.GameMenuButtonAddons then
 		local y, x = tooltip:AddLine()
 		tooltip:SetCell(y, 2, _G.GameMenuButtonAddons:GetText())
 		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.GameMenuButtonAddons)
 	end
-	
-	tooltip:SetAutoHideDelay(0.01, self)
+
+	tooltip:SetAutoHideDelay(1, self)
 	tooltip:SmartAnchorTo(self)
 	tooltip:Show()
 end
